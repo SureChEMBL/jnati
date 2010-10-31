89a0f9e1b48815e4289c5474e5f6d3c267e69a0b
Sam Adams <sea36@users.sf.net>
2010-10-31 15:36:44 +0000

    Updated copyright notices
    git-svn-id: https://svn.code.sf.net/p/jnati/code/jnati/trunk@217 343a5eef-c32a-0410-8a82-bbf7aece9280

--- jnati-maven-plugin/src/main/java/net/sf/jnati/mavenplugin/JnatiMavenPlugin.java
+++ jnati-maven-plugin/src/main/java/net/sf/jnati/mavenplugin/JnatiMavenPlugin.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2008-2010 Sam Adams <sea36 at users.sourceforge.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.sf.jnati.mavenplugin;
 
 import net.sf.jnati.SystemType;
@@ -21,12 +37,6 @@ public class JnatiMavenPlugin extends AbstractMojo {
      * @parameter expression="${project}"
      */
     private MavenProject project;
-
-//    /**
-//     * @parameter expression="${session}"
-//     */
-//    private MavenSession mavenSession;
-
 
     public void execute() throws MojoExecutionException, MojoFailureException {
 
